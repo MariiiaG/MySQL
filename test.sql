c0cc4c84ba50516f3fe1f52c7f4fbcbdd615621a
Mariia <91879850+MariiiaG@users.noreply.github.com>
2023-12-22 13:29:26 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -12,14 +12,17 @@ DECLARE my_days INT DEFAULT 0;
 DECLARE my_hours INT DEFAULT 0;
 DECLARE my_mins INT DEFAULT 0;
 DECLARE remains INT DEFAULT 0;
-DECLARE result VARCHAR(50) DEFAULT 0;
+DECLARE result VARCHAR(200) DEFAULT 0;
 
-SET my_days = seconds / 86400;
+SET my_days = seconds DIV 86400;
 SET remains = seconds - my_days * 86400;
-SET my_hours = remains / 3600;
-SET remains = remains - remains * 3600;
+SET my_hours = remains DIV 3600;
+SET seconds = remains - my_hours * 3600;
+SET my_mins = seconds DIV 60;
+SET remains = seconds - my_mins * 60;
 
-SET result = CONCAT(my_days, " days,", my_hours, " hours", my_mins, "minutes", remains, " seconds");
+
+SET result = CONCAT(my_days, " days, ", my_hours, " hours, ", my_mins, " minutes, ", remains, " seconds");
 
 RETURN result;
    
